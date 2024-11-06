c8a490ad4bb82b5979063279054c019478cb563d
KevinPratamaSetiawan <127008007+KevinPratamaSetiawan@users.noreply.github.com>
2024-11-07 03:59:38 +0700

    uiyuiyui

--- src/components/DeadlineTime.jsx
+++ src/components/DeadlineTime.jsx
@@ -1,36 +1,30 @@
 import React, { useState } from 'react';
 
 const DeadlineTime = ({deadlineTimeData, isToday}) => {
-    const currentHour = new Date().getHours();
-    const currentMinute = new Date().getMinutes() + (currentHour * 60);
-    
+    const now = new Date();
+    const currentTotalMinutes = now.getHours() * 60 + now.getMinutes();
+    const currentTotalSeconds = currentTotalMinutes * 60 + now.getSeconds();
+
     const [timePart, meridiem] = deadlineTimeData.split(' ');
     const [hour, minute] = timePart.split(':').map(Number);
 
-    let deadlineHour;
-    if (meridiem === 'PM' && hour < 12) {
-        deadlineHour = hour + 12;
-    } else if (meridiem === 'AM' && hour === 12) {
-        deadlineHour = 0;
-    } else {
-        deadlineHour = hour;
-    }
-    const deadlineTotalMinutes = minute + (deadlineHour * 60);
+    let deadlineHour = hour % 12 + (meridiem === 'PM' ? 12 : 0);
+    const deadlineTotalMinutes = deadlineHour * 60 + minute;
+    const deadlineTotalSeconds = deadlineTotalMinutes * 60;
 
-    let hourLeft = 0;
-    let minuteLeft = 0;
     let timeLeft = '';
 
-    if(isToday){
-        minuteLeft = deadlineTotalMinutes - currentMinute;
+    if (isToday) {
+        const secondLeft = deadlineTotalSeconds - currentTotalSeconds;
+        const absSecondLeft = Math.abs(secondLeft);
+        const hourLeft = Math.floor(absSecondLeft / 3600);
+        const minuteLeft = Math.floor((absSecondLeft % 3600) / 60);
+        const secondLeftRemaining = absSecondLeft % 60;
 
-        if(minuteLeft > 0){
-            hourLeft = Math.floor(minuteLeft / 60);
-            minuteLeft = minuteLeft % 60;
-
-            timeLeft = ' • ' + (hourLeft > 0 ? hourLeft.toString().padStart(2, '0') + ' hour(s) and ' : '') + minuteLeft.toString().padStart(2, '0') + ' minute(s) left';
-        }else{
-            timeLeft = ' • already passed';
+        if (secondLeft > 0) {
+            timeLeft = ` • ${hourLeft.toString().padStart(2, '0')}h : ${minuteLeft.toString().padStart(2, '0')}m : ${secondLeftRemaining.toString().padStart(2, '0')}s`;
+        } else {
+            timeLeft = ` • ${hourLeft.toString().padStart(2, '0')}h : ${minuteLeft.toString().padStart(2, '0')}m : ${secondLeftRemaining.toString().padStart(2, '0')}s`;
         }
     }
 
